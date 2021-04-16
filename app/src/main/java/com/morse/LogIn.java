5e76d32dbe53f2d46936cc36d513ebc65868f64b
Vlad <visnevskivlad@gmail.com>
2021-04-16 12:35:46 +0300

    temporar LogIn

---
+++ app/src/main/java/com/morse/LogIn.java
@@ -0,0 +1,23 @@
+package com.morse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+
+public class LogIn extends AppCompatActivity {
+    String eMail;
+    String password;
+    int chanelIndex;
+    Image chanelImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+    private void onClick(){
+
+    }
+}
